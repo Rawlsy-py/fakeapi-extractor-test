d11d90ee7e896fd7c03a3de61711b67838cb88b1
chris mitchell <chris@chrismitchell.xyz>
2025-03-19 22:14:47 +0000

    add functionality to upload data to DigitalOcean Space and update dependencies

--- extractor/app.py
+++ extractor/app.py
@@ -2,24 +2,29 @@ import requests
 import os
 import logging
 import polars as pl
+from boto3 import session
+from botocore.client import Config
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 url = os.getenv("URL")
+DO_URL = os.getenv("DO_URL")
+ACCESS_ID = os.getenv("DO_ACCESS_ID")
+SECRET_KEY = os.getenv("DO_SECRET_KEY")
+session = session.Session(region_name="lon1")
 
 
-def extractor(url: str) -> pl.DataFrame:
-    """extract data from API endpoint
+def upload_to_dospace(url: str, df: pl.DataFrame, bucket_name: str, object_name: str) -> None:
+    """Upload raw JSON data directly to DigitalOcean Space
 
     Args:
-        url (str): Url of the API endpoint
-
-    Returns:
-        pl.DataFrame: polars dataframe, nested elements will remain as structs
+        url (str): The URL to extract the raw JSON data
+        data (dict): The raw JSON data to upload
+        bucket_name (str): Name of the DigitalOcean Space bucket
+        object_name (str): Object name in the bucket
     """
-
     try:
         requests.get(url)
         logging.info("API endpoint is up")
@@ -35,30 +40,25 @@ def extractor(url: str) -> pl.DataFrame:
         raise
 
     try:
-        df = pl.DataFrame(data)
-        logging.info("Data converted to polars dataframe")
+        # Upload JSON data to DigitalOcean Space
+        upload = session.client(
+            "s3",
+            region_name="lon1",
+            endpoint_url=DO_URL,
+            aws_access_key_id=ACCESS_ID,
+            aws_secret_access_key=SECRET_KEY,
+        )
+        upload.put_object(
+            Bucket=bucket_name,
+            Key=object_name,
+            Body=data,
+            ContentType="application/json",
+        )
+        logging.info(f"Data uploaded to DigitalOcean Space: {bucket_name}/{object_name}")
     except Exception as e:
-        logging.error("Data conversion to polars dataframe failed")
-        raise SystemExit(e)
-    return df
-
-
-def loader(df: pl.DataFrame, path: str) -> None:
-    """load data to parquet file
-
-    Args:
-        df (pl.DataFrame): polars dataframe
-        path (str): path to save the parquet file
-    """
-    try:
-        df.write_parquet(path)
-        logging.info(f"Data saved to {path}")
-    except Exception as e:
-        logging.error("Data saving failed")
+        logging.error("Data upload to DigitalOcean Space failed")
         raise SystemExit(e)
 
 
 if __name__ == "__main__":
-    df = extractor(url)
-    loader(df, "data.parquet")
-    
+    df = upload_to_dospace(url, "tmp", "my-object")
